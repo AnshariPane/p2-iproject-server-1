b85bd4f1168a961fb7592fe64d2abfed8d6ea9c3
AnshariPane <legion.voidgenome@gmail.com>
2021-09-23 05:38:50 +0700

    socketIO-scuffed

--- controllers/contentController.js
+++ controllers/contentController.js
@@ -1,4 +1,5 @@
-const { Character } = require("../models")
+const { Character } = require("../models");
+const { google } = require("googleapis");
 
 class ContentController {
     static async addCharaForAdmin(req, res, next) {
@@ -8,13 +9,13 @@ class ContentController {
             about: req.body.about,
             fanbaseName: req.body.fanbaseName,
             illustrator: req.body.illustrator,
-            imageUrl: req.body.imageUrl
-        }
+            imageUrl: req.body.imageUrl,
+        };
         try {
-            const newData = await Character.create(data)
-            res.status(201).json(newData)
+            const newData = await Character.create(data);
+            res.status(201).json(newData);
         } catch (error) {
-            next(error)
+            next(error);
         }
     }
 
@@ -22,37 +23,47 @@ class ContentController {
         try {
             const charaData = await Character.findAll({
                 attributes: {
-                    exclude: ["createdAt", "updatedAt",],
+                    exclude: ["createdAt", "updatedAt"],
                     include: ["Generation"],
-                    
                 },
-                order : [
-                    ["id", "ASC"]
-                ]
-            })
-            res.status(200).json(charaData)
+                order: [["id", "ASC"]],
+            });
+            res.status(200).json(charaData);
         } catch (error) {
-            next(error)
+            next(error);
         }
     }
 
     static async findCharaById(req, res, next) {
-        const {id} = req.params
+        const { id } = req.params;
         try {
-            const foundChara = await Character.findByPk(id,
-                {
-                    attributes: {
-                        exclude: ["createdAt", "updatedAt"]
-                    }
-                })
+            const foundChara = await Character.findByPk(id, {
+                attributes: {
+                    exclude: ["createdAt", "updatedAt"],
+                },
+            });
             if (!foundChara) {
-                throw ({name: "not found"})
+                throw { name: "not found" };
             }
-            res.status(200).json(foundChara)
+            res.status(200).json(foundChara);
         } catch (error) {
-            next(error)
+            next(error);
+        }
+    }
+
+    static async getData(req, res, next) {
+        try {
+            const response = await google.youtube("v3").search.list({
+                key: process.env.GOOGLEAPI,
+                q: req.query.name,
+                part: "snippet",
+            });
+            res.status(200).json(response.data.items);
+        } catch (error) {
+            console.log(error,"````````")
+            next(error);
         }
     }
 }
 
-module.exports = ContentController
+module.exports = ContentController;
